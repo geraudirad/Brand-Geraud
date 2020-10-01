0b5f5861ade649ef4bda254467fb5d19f548c10e
Geraud Iradukunda <geraudiradukunda@gmail.com>
2020-10-01 12:15:28 +0200

    Tests of endpoints

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from "express"
-import mongoose from "mongoose"// new
+import mongoose from "mongoose"
 import routes from "./routes"
 
+const app = express();
 mongoose
 	.connect("mongodb://localhost:27017/blog", { useNewUrlParser: true,useUnifiedTopology: true })
 	.then(() => {
-		const app = express();
 		app.use(express.json()); // new
 		app.use("/api/", routes);
 
@@ -13,3 +13,4 @@ mongoose
 			console.log("Server has started!");
 		});
 	})
+export default app;
