5f06c1903636b4e723e9af6d4f48fa2d5f405ddd
Geraud Iradukunda <geraudiradukunda@gmail.com>
2020-10-04 11:32:19 +0200

    Added Config Port

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 import express from "express"
 import mongoose from "mongoose"
+import ENV from "dotenv";
 import routes from "./routes"
 import swaggerUi from "swagger-ui-express"
 import swaggerDocument from "../swagger.json"
 
 const app = express();
+ENV.config();
+
 mongoose
-	.connect("mongodb://localhost:27017/blog", { useNewUrlParser: true,useUnifiedTopology: true })
+	.connect(process.env.MONGO_URL, { useNewUrlParser: true,useUnifiedTopology: true })
 	.then(() => {
 		app.use(express.json()); // new
 		app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 		app.use("/api/", routes);
 
-		app.listen(5000, () => {
-			console.log("Server has started!");
+		app.listen(process.env.PORT, () => {
+			console.log(`Server has started! Running on ${process.env.PORT}`);
 		});
 	})
 export default app;
